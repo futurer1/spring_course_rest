65de3a7ef7af09b282b107265e7fb0baae4871c5
Mikhail Afonin <mafonin@sweb.ru>
2023-11-17 18:56:50 +0300

    REST API. Handling Exceptions

--- src/main/java/com/mikhail/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/mikhail/spring/rest/controller/MyRESTController.java
@@ -1,8 +1,12 @@
 package com.mikhail.spring.rest.controller;
 
 import com.mikhail.spring.rest.entity.Employee;
+import com.mikhail.spring.rest.exception_handling.EmployeeIncorrectData;
+import com.mikhail.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.mikhail.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,7 +27,32 @@ public class MyRESTController {
     @GetMapping("/employees/{id}")
     public Employee showEmployee(@PathVariable int id) {
         Employee emp = employeeService.getEmployee(id);
+
+        if (emp == null) {
+            throw new NoSuchEmployeeException("Employee is not find with ID = " + id);
+        }
+
         return emp;
+    }
+
+    @ExceptionHandler // ловит все исключения типа NoSuchEmployeeException
+    public ResponseEntity<EmployeeIncorrectData> /*Объект класса EmployeeIncorrectData будет преобразован в JSON*/ handleException(
+            NoSuchEmployeeException exception // метод будет реагировать на этот тип Exception
+    ) {
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(
+            Exception exception
+    ) {
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+
+        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
     }
 
 }
---
+++ src/main/java/com/mikhail/spring/rest/exception_handling/NoSuchEmployeeException.java
@@ -0,0 +1,8 @@
+package com.mikhail.spring.rest.exception_handling;
+
+public class NoSuchEmployeeException extends RuntimeException {
+
+    public NoSuchEmployeeException(String message) {
+        super(message);
+    }
+}
