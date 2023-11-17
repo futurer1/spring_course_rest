08c59a469032a662393acf8e38157ca38518443f
Mikhail Afonin <futurer@inbox.ru>
2023-11-17 19:04:28 +0300

    REST API. Handling Exceptions

--- src/main/java/com/mikhail/spring/rest/exception_handling/NoSuchEmployeeException.java
+++ src/main/java/com/mikhail/spring/rest/exception_handling/NoSuchEmployeeException.java
@@ -1,7 +1,6 @@
 package com.mikhail.spring.rest.exception_handling;
 
 public class NoSuchEmployeeException extends RuntimeException {
-
     public NoSuchEmployeeException(String message) {
         super(message);
     }
